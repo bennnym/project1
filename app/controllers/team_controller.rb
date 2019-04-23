b0aacade9f376f7875982d8be756569cfd92dae6
Ben Muller <benmuller@Bens-Macbook.modem>
2019-04-23 19:07:35 +1000

    handled input

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -9,10 +9,17 @@ class TeamController < ApplicationController
   def edit
     @team = Team.find_by :id => session[:team_id]
     
+    if params[:name].split(' ').length == 1 || params[:name].empty?
+      return redirect_to team_path
+    end
+        
     name = Team.new
     name = name.validate_player params[:name] #returns a hash with first and last names stored
     
     player = Player.find_by :last_name => name[:last_n], :first_name => name[:first_n]
+    
+    return redirect_to team_path if player.nil?
+    
     @current_user.players << player
     render :show  
   end
