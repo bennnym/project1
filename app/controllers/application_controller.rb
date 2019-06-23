16bdd4c365b592dfb328e13cbce960c65d3e3c21
Ben Muller <ben.k.muller@gmail.com>
2019-06-24 09:42:06 +1000

    removed live scores

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :fetch_user, :scores_nav # first action the server will take
+  before_action :fetch_user # :scores_nav # first action the server will take
 
   private
 
@@ -15,22 +15,22 @@ class ApplicationController < ActionController::Base
   end
 
   ### This makes the score api request
-  def get_scores
-    api = Rails.application.secrets.live_scores_api
-    date = (DateTime.now.utc - 1).strftime('%Y-%m-%d')
-    url = "https://allsportsapi.com/api/basketball/?met=Fixtures&APIkey=#{api}&from=#{date}&to=#{date}&leagueId=787"
-    info = HTTParty.get url
-    games = info['result']
+  # def get_scores
+  #   api = Rails.application.secrets.live_scores_api
+  #   date = (DateTime.now.utc - 1).strftime('%Y-%m-%d')
+  #   url = "https://allsportsapi.com/api/basketball/?met=Fixtures&APIkey=#{api}&from=#{date}&to=#{date}&leagueId=787"
+  #   info = HTTParty.get url
+  #   games = info['result']
 
-    games.each do |game|
-      score = Score.new
-      score.home_team = game['event_home_team']
-      score.home_score = game['event_final_result'].first(3)
-      score.away_team = game['event_away_team']
-      score.away_score = game['event_final_result'].last(3)
-      score.save
-    end
-  end
+  #   games.each do |game|
+  #     score = Score.new
+  #     score.home_team = game['event_home_team']
+  #     score.home_score = game['event_final_result'].first(3)
+  #     score.away_team = game['event_away_team']
+  #     score.away_score = game['event_final_result'].last(3)
+  #     score.save
+  #   end
+  # end
 
   # this just makes sure we are doing it once a day only and getting images also
   # def scores_nav
@@ -42,14 +42,14 @@ class ApplicationController < ActionController::Base
   #     get_scores if Time.now > Time.parse("#{Date.today} 16:00") && last_game_update + 86_400 < Time.now.utc
   #   end
 
-    @images = []
-    @games = Score.last(3)
-    @games.each do |game|
-      away = game.away_team.split(' ').last
-      away_img = Team.where('name like ?', "%#{away}%").first.logo
-      home = game.home_team.split(' ').last
-      home_img = Team.where('name like ?', "%#{home}%").first.logo
-      @images.push([home_img, away_img])
-    end
-  end
+  #   @images = []
+  #   @games = Score.last(3)
+  #   @games.each do |game|
+  #     away = game.away_team.split(' ').last
+  #     away_img = Team.where('name like ?', "%#{away}%").first.logo
+  #     home = game.home_team.split(' ').last
+  #     home_img = Team.where('name like ?', "%#{home}%").first.logo
+  #     @images.push([home_img, away_img])
+  #   end
+  # end
 end
